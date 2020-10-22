681125e2a6b080ba0ccdd227d23d57c3b59d99f7
Aaronrss <A01335324@itesm.mx>
2020-10-22 17:31:14 -0500

    checkHit
    video melee en clase

--- UFG/Assets/Scripts/PlayerAttack.cs
+++ UFG/Assets/Scripts/PlayerAttack.cs
@@ -8,16 +8,33 @@ public class PlayerAttack : MonoBehaviour
     public float[] AttackStrength;
     public float[] AttackDelay;
     float NextAttack = 0;
-    
+    public Transform attackPoint;
+    public float attackRange;
+    public LayerMask enemyLayers;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public void checkHit(){
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("Weee" + enemy.name);
+            PlayerHealth ph = enemy.gameObject.GetComponent <PlayerHealth>();
+            if(ph != null)
+            {
+                ph.TakeDamage(20);
+            }
+        }
+        
+    }
     // Update is called once per frame
     void Update()
     {
+
 
         if(Input.GetKeyDown(KeyCode.Space) && Time.time > NextAttack)
         {
