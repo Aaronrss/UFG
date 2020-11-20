0faf6d4631df9f61dc0b608ffc9f87929ea906f9
Aaronrss <A01335324@itesm.mx>
2020-11-19 20:01:59 -0600

    asesoria profe

---
+++ UFG/Assets/Scripts/BackgroundChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundChanger : MonoBehaviour
+{
+    public Sprite[] Bg;
+    public SpriteRenderer Ren;
+    public int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = PlayerPrefs.GetInt("Stage");
+        Ren = GetComponent<SpriteRenderer>();
+        Ren.sprite = Bg[index];
+    }
+}
