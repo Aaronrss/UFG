431f318f7d3edf78691a6515c604b46afb00ba7d
charliedelagarza <chariedelagarza@gmail.com>
2020-11-22 22:48:05 -0600

    Character Selecton
    Character Selecton

---
+++ UFG/Assets/Scripts/FighterSelection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterSelection : MonoBehaviour
+{
+
+    public Fighters[] fighters;
+    public Transform Spot;
+
+    private List<GameObject> fighters1; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fighters1 = new List<GameObject>();
+
+        foreach (var fighter in fighters)
+        {
+            GameObject go = Instantiate(fighter.fighter, Quaternion.identity);  
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UFG/Assets/Scripts/Fighters.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Fighters", menuName = "Fighter")]
+public class Fighters : ScriptableObject
+{
+    public string Name;
+
+    public float Strenght;
+
+    public float Speed;
+
+    public float Health;
+
+    public float Reach;
+
+    public GameObject fighters1; 
+}
