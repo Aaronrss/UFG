e7b05c835139c330436ff39c51f415e7c1fa973f
Rodrigo Márquez <44romanu@gmail.com>
2020-11-26 15:50:49 -0600

    prefs
    player prefs

---
+++ UFG/Assets/Scripts/Prefs.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Prefs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void PlayerSelect(int players)
+    {
+        PlayerPrefs.SetInt("Players", players);
+        SceneManager.LoadScene("CharacterSelect");
+    }
+    public void CharacterSelect(int character)
+    {
+        PlayerPrefs.SetInt("Character1", character);
+    }
+    public void StageSelect(int stage)
+    {
+        PlayerPrefs.SetInt("Stage", stage);
+        SceneManager.LoadScene("Fight");
+    }
+}
