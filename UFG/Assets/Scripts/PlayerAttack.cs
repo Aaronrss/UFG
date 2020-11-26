6ab746c662f138697547e5da189b789557937ab3
Rodrigo Márquez <44romanu@gmail.com>
2020-11-26 12:07:43 -0600

    damage
    menos daño por golpe

--- UFG/Assets/Scripts/PlayerAttack.cs
+++ UFG/Assets/Scripts/PlayerAttack.cs
@@ -32,7 +32,7 @@ public class PlayerAttack : MonoBehaviour
             PlayerHealth ph = enemy.gameObject.GetComponent <PlayerHealth>();
             if(ph != null)
             {
-                ph.TakeDamage(20);
+                ph.TakeDamage(10);
                 ownPlayerStamina.IncreaseStamina(20);
             }
         }
